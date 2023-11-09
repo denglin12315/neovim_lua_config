c09a8e11dea96e50f581c3113df4d1c9dd25b00f
ldeng <denglin12315@163.com>
2023-11-09 17:01:43 +0800

    add lsp support
    Signed-off-by: ldeng <denglin12315@163.com>

--- init.lua
+++ init.lua
@@ -1,14 +1,6 @@
 require("basic")
 require("keybindings")
-require("plugins")
+require("plugin.plugins")
 require("colorscheme")
-
--- 插件配置
-require("plugin-config.nvim-tree")
-require("plugin-config.bufferline")
-require("plugin-config.lualine")
-require("plugin-config.telescope")
-require("plugin-config.dashboard")
-require("plugin-config.project")
-
+require("lsp.setup")
 
--- lua/plugin/plugins.lua
+++ lua/plugin/plugins.lua
@@ -3,21 +3,53 @@ packer.startup(
 function(use)
 	-- Packer可以管理自己本身
 	use 'wbthomason/packer.nvim'
+
 	-- 主题插件
 	use 'tanvirtin/nvim-monokai'
+
 	-- 侧边栏文件浏览器
 	use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })
+
 	-- 标签栏
 	use({ "akinsho/bufferline.nvim", requires = { "kyazdani42/nvim-web-devicons", "moll/vim-bbye" }})
+
 	-- 状态栏
 	use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons" } })
 	use("arkav/lualine-lsp-progress")
+
 	-- telescope & telescope extension
 	use({ 'nvim-telescope/telescope.nvim', requires = { "nvim-lua/plenary.nvim" }})
 	use "LinArcX/telescope-env.nvim"
+
 	-- dashboard
 	use({ 'nvimdev/dashboard-nvim', requires = {'nvim-tree/nvim-web-devicons'}})
 	use("ahmedkhalf/project.nvim")
+
+	-- LSP-server
+	use({ "williamboman/mason.nvim" })
+	use({ "williamboman/mason-lspconfig.nvim" })
+	-- LSP-client
+	use({ "neovim/nvim-lspconfig" })
+
+	-- 补全引擎
+	use("hrsh7th/nvim-cmp")
+	-- Snippet 引擎
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
+	-- 补全源
+	use("hrsh7th/cmp-vsnip")
+	use("hrsh7th/cmp-nvim-lsp") -- { name = nvim_lsp }
+	use("hrsh7th/cmp-buffer") -- { name = 'buffer' },
+	use("hrsh7th/cmp-path") -- { name = 'path' }
+	use("hrsh7th/cmp-cmdline") -- { name = 'cmdline' }
+	use("hrsh7th/cmp-nvim-lsp-signature-help") -- { name = 'nvim_lsp_signature_help' }
+
+	-- Lua 增强
+	use("folke/neodev.nvim")
+	-- JSON 增强
+	use("b0o/schemastore.nvim")
+	-- Rust 增强
+	use("simrat39/rust-tools.nvim")
 end)
 
 -- 每次保存 plugins.lua 自动安装插件
@@ -30,4 +62,12 @@ pcall(
     augroup end
   ]]
 )
+
+require("plugin.config.nvim-tree")
+require("plugin.config.bufferline")
+require("plugin.config.lualine")
+require("plugin.config.telescope")
+require("plugin.config.dashboard")
+require("plugin.config.project")
+
 
